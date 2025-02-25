888be5ccbd561c8cabb27b61f527eb73a5b7ec27
AmandinhaF1 <amanda.alibosek@escola.pr.gov.br>
2025-02-25 07:27:39 -0300

    Add files via upload

---
+++ fibonacci.js
@@ -0,0 +1,8 @@
+function fibonacci(n) {
+let seq = [0, 1];
+while (seq[seq.length - 1] + seq[seq.length - 2] < n){
+    seq.push(seq[seq.length - 1] + seq[seq.length - 2]);
+}
+console.log(seq);
+}
+fibonacci(100);
---
+++ inverter.js
@@ -0,0 +1,3 @@
+let str = "amanda";
+let invertida = str.split("").reverse().join("");
+console.log(invertida);
