be2399cc19e9e466b841770b3b80644bf94a2320
AmandinhaF1 <amanda.alibosek@escola.pr.gov.br>
2025-03-17 08:08:53 -0300

    Add files via upload

---
+++ calculadora/cadastro.js
@@ -0,0 +1,8 @@
+function cadastro() {
+    let num1 = parseFloat(document.getElementById("CPF").value);
+    let num2 = parseFloat(document.getElementById("Nome Completo").value);
+    let num3 = parseFloat(document.getElementById("Idade").value);
+    let num4 = parseFloat(document.getElementById("RG").value);
+    let num5 = parseFloat(document.getElementById("Endereço").value);
+    let num6 = parseFloat(document.getElementById("Profissão").value);
+}
